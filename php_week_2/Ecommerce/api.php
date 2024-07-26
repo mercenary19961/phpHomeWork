acde09ba81afeba303cf23a8f9db13ddbc1c6d94
Zaid <sabbaghzaid88@gmail.com>
2024-07-26 22:08:41 +0300

    ecommerce

--- php_week_2/Ecommerce/api.php
+++ php_week_2/Ecommerce/api.php
@@ -1,27 +1,17 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+include('config.php');
 
-$method = $_SERVER['REQUEST_METHOD'];
+$search = isset($_GET['search']) ? $_GET['search'] : '';
 
-if ($method == 'GET') {
-    // Handle GET request
-    $data = [
-        'message' => 'Hello, this is a GET request',
-        'status' => 'success'
-    ];
-    echo json_encode($data);
-} elseif ($method == 'POST') {
-    // Handle POST request
-    $input = json_decode(file_get_contents('php://input'), true);
-    $data = [
-        'message' => 'Hello, this is a POST request',
-        'received' => $input,
-        'status' => 'success'
-    ];
-    echo json_encode($data);
-} else {
-    // Handle other request methods
-    http_response_code(405);
-    echo json_encode(['message' => 'Method Not Allowed']);
+$query = "SELECT * FROM products";
+if ($search) {
+    $query .= " WHERE name LIKE '%$search%'";
 }
+
+$result = mysqli_query($conn, $query);
+$products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+echo json_encode($products);
 ?>
--- php_week_2/Ecommerce/create.php
+++ php_week_2/Ecommerce/create.php
@@ -1,21 +1,23 @@
 <?php
+error_reporting(0);
+
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Request-With");
 
 include('function.php');
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 
-if ($requestMethod == "GET") {
-    if (isset($_GET["id"])) {
-        $customer = getCustomer($_GET);
-        echo $customer;
+if ($requestMethod == "POST") {
+    $inputData = json_decode(file_get_contents("php://input"), true);
+    if (empty($inputData)) {
+        $storedCustomer = storeCustomer($_POST);
     } else {
-        $customerList = getCustomerList();
-        echo $customerList;
+        $storedCustomer = storeCustomer($inputData);
     }
+    echo $storedCustomer;
 } else {
     $data = [
         'status' => 405,
